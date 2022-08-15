b0fdd233978f624de9096186824a9c621dc6413d
Robert Nelson <robertcnelson@gmail.com>
2022-08-15 10:37:25 -0500

    6.0-rc1-omap2plus-r0 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -8,7 +8,7 @@ build_prefix="-omap2plus-r"
 branch_prefix="v"
 branch_postfix=".x"
 
-#https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/tree/Documentation/process/changes.rst?h=v5.19-rc1
+#https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/tree/Documentation/process/changes.rst?h=v6.0-rc1
 #arm
 KERNEL_ARCH=arm
 DEBARCH=armhf
@@ -40,13 +40,13 @@ toolchain="gcc_12_arm"
 #toolchain="gcc_12_riscv64"
 
 #Kernel
-KERNEL_REL=5.19
-KERNEL_TAG=${KERNEL_REL}
+KERNEL_REL=6.0
+KERNEL_TAG=${KERNEL_REL}-rc1
 kernel_rt=".X-rtY"
 #Kernel Build
 BUILD=${build_prefix}0
 
-#v5.X-rcX + upto SHA
+#v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
 #KERNEL_SHA=""
 
